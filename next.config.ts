66247752200f9c1bc7226e0e628a42dd90ae60bb
AndresDev28 <andresdev28@hotmail.com>
2025-08-22 11:38:52 +0200

    chore(images): remove unused category images

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'relojes-bv-beni-api.onrender.com',
-        pathname: '/uploads/**',
-      },
+      // {
+      //   protocol: 'http',
+      //   hostname: 'relojes-bv-beni-api.onrender.com',
+      //   pathname: '/uploads/**',
+      // },
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
